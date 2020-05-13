9c5d4379d5ae14874c8ba1daa47ac2c453a86c66
rachelwen <rachelwen823@gmail.com>
2020-05-12 21:14:22 -0700

    added theme of chinese alphabet soup

--- particle.js
+++ particle.js
@@ -1,21 +1,29 @@
 class Particle {
     constructor(x, y) {
-        this.pos = createVector(x, y);
-        //this.pos = p5.Vector.random2D(); //particles fly in -> form stationary face
+       // this.pos = createVector(x, y);
+        this.pos = p5.Vector.random2D(); //particles fly in -> form stationary face
         this.prevPos = this.pos.copy();
         this.target = createVector(x, y); // =p5.Vector.random2D(); particles sucked out to top right of screen
-        // this.target = this.pos;
+         //this.target = p5.Vector.random2D();
        // this.target = this.prevPos;
         this.vel = p5.Vector.random2D();
         this.acc = createVector();
 
-        this.maxSpeed = 5;
-        this.maxForce = 2; //controls how well the particle can return to its position
+        this.maxSpeed = 10;
+        this.maxForce = 0.5; //controls how well the particle can return to its position
         this.alpha = 255;
+        this.characters = "一二三十木禾上下土个八入大天人火文六七儿九无口日";
+        for(let i = 0; i < this.characters.length; i++){
+            this.randomCharacter = this.characters[floor(random(this.characters.length))];
+            return this.randomCharacter;
+        }
+       
+        
+        
     }
 
     update() {
-        // this.alpha -= 205; // fades out particles
+         this.alpha -= 55; // fades out particles
         this.pos.add(this.vel);
         this.vel.add(this.acc);
         this.acc.mult(0); //acc accumulates forces, every frame needs to start from zero
@@ -23,18 +31,39 @@ class Particle {
     }
 
     show() {
-        stroke(random(35), random(40, 80), 80);
-        strokeWeight(random(4, 8));
-        line(this.pos.x, this.pos.y,this.prevPos.x,this.prevPos.y);
-       // this.updatePrev(); 
+       // this.hueVal = floor(random(35));
+        this.satVal = floor(random(40,80));
+        
+        
+        stroke(random(35),random(40,80), 50);
+       
+    
+       // strokeWeight(random(4,8));
+       strokeWeight(1)
+       // line(this.pos.x, this.pos.y,this.prevPos.x,this.prevPos.y);
+       this.updatePrev(); 
+       noFill()
+       //ellipse(this.pos.x,this.pos.y,5,5)
+      
+      // text(alphabet[randomLetter],this.pos.x,this.pos.y,20,20)
+      text(this.randomCharacter,this.pos.x,this.pos.y,20,20)
+       
+       
+        
     }
 
     toDelete() { //this looks to see if the alpha is less than zero
         return this.alpha < 0;
     }
 
+    // randomLetter(){
+    //     for(let i = 0; i < this.alphabet.length; i++){
+    //         randomLetter = floor(random(alphabet.length));
+    //         return this.alphabet[randomLetter];
+    //     }
+    // }
   
-    
+   
 
     updatePrev(){
         this.prevPos.x = this.pos.x;
@@ -79,14 +108,22 @@ class Particle {
         //need to find vector from obj location to the place its seeking
         let desired = p5.Vector.sub(target, this.pos); //subtract to find vector that points from position to target
         let d = desired.mag();
-        console.log(d)
-        if (d < 400) { // particles only react to mouse when this close
+       
+        if (d < 50) { // particles only react to mouse when this close
             desired.mult(-1);
+            // push()
+            //     fill(255)
+            //     translate(width,0)
+            //     scale(-1,1)
+            //     text('hovered',100,100)
+            // pop()
+        
             desired.setMag(this.maxSpeed);
-            console.log('mouse on face');
+    
             // changes vector to go in opposite direction
             //steering = desired - velocity:
             let steer = p5.Vector.sub(desired, this.vel);
+          //  let steer = p5.Vector.sub(desired, this.pos)
             steer.limit(this.maxForce);
             return steer;
         } else {
--- sketch.js
+++ sketch.js
@@ -3,45 +3,75 @@ var tracker;//
 let fr;
 let particles = [];
 //let features = null; // list of facial features
-var w = 640;
-var h = 480;
+var w = 400;
+var h = 400;
+let randomColor;
+let randomLetter = [];
 let mouth = [44, 61, 60, 59, 50, 58, 57, 56] // points of outside of mouth taken from reference
 ////www.auduno.com/clmtrackr/docs/reference.html
 let prevPos = [];
-function setup(){
+// let alphabet = "abcdefghijklmnopqrstuvwxyz";
+let alphabet;
+let backgroundImage;
+
+function setup() {
+    backgroundImage = loadImage('sqplate.png');
     webcam = createCapture({
         audio: false,
         video: {
             width: w,
             height: h
         }
-    }, function() {
+    }, function () {
         console.log('webcam ready.')
     });
     webcam.elt.setAttribute('playsinline', '');
-    createCanvas(w, h);
+    let cnv =createCanvas(w, h);
+    let x = (windowWidth-width)/2;
+    let y = (windowHeight-height)/2;
+    cnv.position(x,y);
     webcam.size(w, h);
     webcam.hide();
-    
+
     colorMode(HSB);
 
-   fr = createP('');
-   
+    fr = createP('');
+    noCursor()
 
+//     for(let i = 0; i < alphabet.length; i++){
+//         randomLetter = floor(random(alphabet.length));
+//    }
+  
     //connect facetracking to web cam
     tracker = new clm.tracker();
     tracker.init();
     tracker.start(webcam.elt); // connects tracker
+    randomColor = floor(random(30, 360));
+
     
+     
+  
+   
+   
+   
 }
 
-function draw(){
-    background(203, 56, 35);
-    translate(width,0)// reflect video
-    scale(-1,1);
-   //image(webcam,0,0,w,h);
+function draw() {
+   
 
-         var positions = tracker.getCurrentPosition();
+    //background(203, 56, 35);
+    background(0,86,65);
+    //image(backgroundImage,0,0,w,h);
+    
+     bowl();
+     chopsticks(mouseX, mouseY)
+    
+    
+    // translate(width,0)// reflect video
+    // scale(-1,1);
+    //image(webcam,0,0,w,h);
+
+     var positions = tracker.getCurrentPosition();
     // if(frameCount % 100 == 0){
     //     tracker.reset();
     // }
@@ -54,32 +84,102 @@ function draw(){
 
     noFill();
     //if(random(1)<0.1){ // makes face 'flashy'
+
     for (let i = 0; i < positions.length; i++) {
-        //point(positions[i][0],positions[i][1])
-      //  let dot = positions[i];
-      
-       let particle = new Particle(positions[i][0],positions[i][1]);
+
+
+        let particle = new Particle(positions[i][0], positions[i][1]);
         particles.push(particle); //adds particle to particle array
         
-     }
-   // }
-    for(let j = 0; j < particles.length; j++ ){ // two faces show up bc for loops starts at end
+        // ellipse(positions[62][0],positions[62][0],200,20);
+
+    }
+   
+
+
+
+
+    // }
+    for (let j = particles.length - 1; j > 0; j--) { // two faces show up bc for loops starts at end
+    
         
         particles[j].behaviors();
         particles[j].show();
         particles[j].update();
-       
-        
+            
+
+
+
+
         // if(particles[j].toDelete()){
         //     // remove this particle
         //     particles.splice(j,1); // from coding train simple particle system tutorial
         // }
-        if(particles.length > 71){ // if >0, stationary face will be hidden
-            particles.splice(j,1);
+        if (particles.length > 71) { // if >0, stationary face will be hidden
+            particles.splice(j, 1);
+
         }
-        
+
+        //if(millis() > 3000){
+        //tracker.stop(webcam.elt)
+        //particles.splice(j,1);
+        // }
+
     }
-   
     fr.html(floor(frameRate()));
 
 }
+
+function chopsticks(x, y) {
+    strokeWeight(5)
+    stroke(0, 51, 35)
+    // line(x + 45, y - 10, x - 30, y + 20 - random(5))
+    // line(x + 50, y, x - 25, y + 20 + random(5))
+    line(x + 65, y - 30, x - 50, y + 40 - random(5))
+     line(x + 70, y, x - 45, y + 40 + random(5))
+
+}
+
+function stick(x, y) {
+    strokeWeight(4)
+    stroke(0, 51, 35)
+    line(x, y - 5, x - 50, y + 40)
+    push()
+    beginShape()
+    noStroke()
+    fill('green');
+    vertex(x - 10, y + 0)
+    vertex(x - 15, y - 10)
+    vertex(x - 20, y + 10)
+    endShape()
+    pop()
+}
+
+function bowl() {
+    
+    push()
+        fill(0,0,20)
+        noStroke()
+        ellipse((-5+width/2),(5+height/2),w-10,h-10);
+    pop();
+    strokeWeight(1);
+    push()
+        fill(0,0,90)
+        stroke(0,0,80);
+        ellipse(width / 2, height / 2,w-15,h-15);
+    pop()
+
+    push()
+        noFill()
+        stroke(228,69,93);
+        ellipse(width / 2, height / 2,w-30,h-30);
+    pop()
+
+    push()
+        fill(48,99,92)
+        stroke(0,0,80);
+        ellipse(width / 2, height / 2,w-100,h-100);
+    pop()
+    
+}
+
